3a7721bc7e8023487895b7b682a417d1fb04d3b2
DESKTOP-D5DFVFJ\karth <karthiksai.0812@gmail.com>
2026-04-28 11:00:14 +0530

    feat : add entities

---
+++ Domain/Entities/OtpRecord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class OtpRecord
+    {
+        public int OtpId { get; set; } 
+        public int UserId { get; set; }
+        public string Code { get; set; }
+        public DateTime ExpiresAt { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ Domain/Entities/SmtpSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class SmtpSettings
+    {
+        public string Host { get; set; } = string.Empty;
+        public int Port { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string FromName { get; set; } = "Auth Service";
+        public bool UseSsl { get; set; } = true;
+    }
+}
