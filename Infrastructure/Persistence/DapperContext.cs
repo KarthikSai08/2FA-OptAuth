5bc0ca55a2055706e716656ecfcad54ae443cbe4
DESKTOP-D5DFVFJ\karth <karthiksai.0812@gmail.com>
2026-04-28 11:29:00 +0530

    feat : add DapperContext

---
+++ Infrastructure/Persistence/DapperContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace Infrastructure.Persistence
+{
+    public class DapperContext
+    {
+        private readonly string _con;
+        public DapperContext(IConfiguration config)
+        {
+            _con = config.GetConnectionString("DefaultConnection")
+                ?? throw new ArgumentNullException("Connection String not Found");
+        }
+        public IDbConnection CreateConection() => new SqlConnection(_con);
+    }
+}
